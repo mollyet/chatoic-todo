24ce6c1c3d4672c57a459813a587b2ca6910e598
Molly Trepanier <mtrepanier123@gmail.com>
2022-04-07 16:10:45 -0700

    fiat scriptum-- took out undeed files, add hobbits

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=uE-1RPDqJAY"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         they're takign the hobbits to isengard
         </a>
+        </h1>
       </header>
     </div>
   );
